278aa75f0d0fda46903d3486cb0bd3fa7ff0c0c2
hadenaeter <hadenaeter@gmail.com>
2025-03-10 14:16:10 +0400

    add player movement

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
